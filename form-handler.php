5f71f062b22aa61eee842b554869c1a2d8394925
Shambhu Leakhni <71211556+ShambhuLeakhni@users.noreply.github.com>
2021-06-16 08:52:11 +0530

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'razzabhaii190@gmail.com';
+$email_from = 'info@shambhuleakhni.com';
 
 $email_subject = 'New Form Submission';
 
@@ -12,7 +12,7 @@ $email_body = "User Name: $name.\n".
               "User Email: $visitor_email.\n".
                "Subject: $subject.\n".
                "User Message: $message .\n";
-$to = 'singhsumitraj3@gmail.com';
+$to = 'razzabhaii190@gmail.com.com';
 
 $headers = "From: $email_from \r\n";
 
